fe8073dc43dd67f891f5e892c0a4d112a99f7748
taekPro <hotaekjang@192.168.nate.com>
2023-01-20 17:42:40 +0900

    first test

---
+++ CodingTest/CodingTest/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  CodingTest
+//
+//  Created by Hotaek Jang on 2023/01/20.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ CodingTest/Solutions/양궁 대회.swift
@@ -0,0 +1,8 @@
+//
+//  양궁 대회.swift
+//  CodingTest
+//
+//  Created by Hotaek Jang on 2023/01/20.
+//
+
+import Foundation
